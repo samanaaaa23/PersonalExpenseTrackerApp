d3a2b3b4787ccc92833d6108519bd6e4de797c4a
samanab <samanabhatta44@gmail.com>
2024-12-18 22:57:44 +0545

    Initial commit: Added Visual Studio project

---
+++ PersonalExpenseTrackerApp/PersonalExpenseTrackerApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalExpenseTrackerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
